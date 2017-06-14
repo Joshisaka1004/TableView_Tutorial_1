45c0bb3b6eac1380f414a8802d9d35691aeb1b9c
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-14 19:42:18 +0200

    Very basic state with hard-coded 5 rows and all have the same (also hard-coded text "Kakuro"

--- myTableViewTutorial_1_170614/ViewController.swift
+++ myTableViewTutorial_1_170614/ViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// In this example I use a UITableViewController, so I've deleted the commom UIViewController from the IB and replaced it by the pre-configured UITableViewController from the object library, I call my correspondig class for this controller "myTableViewController" as you see below, and it inherits from UITableViewController; don't forget to give this new Controller the correct class name (myTableViewController) in the Identity inspector (top row there) and to set it as the Initial View Controller in the Attribute Inspector.
+
+class myTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //we don't need delegate declarations with such a UITableViewController, it knows how to ask for the data source and also knows how to treat/react to user events with our table!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //But we MUST implement two methods to make our table work at all:
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
-
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCells = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        myCells.textLabel?.text = "Kakuro"
+        return myCells
+    }
 }
 
