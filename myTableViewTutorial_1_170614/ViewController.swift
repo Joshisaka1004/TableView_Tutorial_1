d55612d0c9e286f83e94c22870552f27b9b3e1be
Joachim Vetter <JoachimVetter@p200300e723c8e7fc04ecf905cf062a34.dip0.t-ipconnect.de>
2017-06-14 23:04:10 +0200

    added the class for our (prototype) table view cell

--- myTableViewTutorial_1_170614/ViewController.swift
+++ myTableViewTutorial_1_170614/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 // In this example I use a UITableViewController, so I've deleted the commom UIViewController from the IB and replaced it by the pre-configured UITableViewController from the object library, I call my correspondig class for this controller "myTableViewController" as you see below, and it inherits from UITableViewController; don't forget to give this new Controller the correct class name (myTableViewController) in the Identity inspector (top row there) and to set it as the Initial View Controller in the Attribute Inspector.
 
+// We can either make a separate swift file containing a class for the table view cell object OR we can leave this away by giving a tag to the view(s) inside this prototype cell; first, I show how to use a separate class for this prototype cell, I call it myCellViews!
+
 class myTableViewController: UITableViewController {
 
     override func viewDidLoad() {
@@ -22,8 +24,12 @@ class myTableViewController: UITableViewController {
         return 5
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let myCells = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        myCells.textLabel?.text = "Kakuro"
+        let myCells = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! myCellView
+        //above: the reusable cells of our table with the specified identifier and the specified index path is now downcasted as "myCellView", this means that our protoype cell must now be referenced to via our created class for this table view cell. The constant "myCells" is now the reference to the reusable (protoype) cells which are defined via our class "myCellView"
+        myCells.myLabel.text = "Kakuro"
+        //And now we refer to the Outlet variable "myLabel" in this class for the rable view cell. We have access to this variable because we created the reference above via the variable "myCells" and the downcasting "as! myCellView".
+        myCells.accessoryType = .none
+        //At the beginning we do not have any checkmarks for our rows.
         return myCells
     }
 }
---
+++ myTableViewTutorial_1_170614/myCellView.swift
@@ -0,0 +1,22 @@
+//
+//  myCellView.swift
+//  myTableViewTutorial_1_170614
+//
+//  Created by Joachim Vetter on 14.06.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class myCellView: UITableViewCell {
+
+    
+    @IBOutlet weak var myLabel: UILabel!
+    //We create an I Outlet connection to our prototype cell object in the IB and call this variable "myLabel"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
