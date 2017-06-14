b422a252956c841b36eaaa1b8c35fd19bbc3533f
Joachim Vetter <JoachimVetter@joachims-mbp>
2017-06-14 17:24:25 +0200

    Initial Commit

---
+++ myTableViewTutorial_1_170614/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myTableViewTutorial_1_170614
+//
+//  Created by Joachim Vetter on 14.06.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
