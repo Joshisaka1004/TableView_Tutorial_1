7c2cb03e4ac1f630958ec815da2c8e0a7fd27f52
Joachim Vetter <JoachimVetter@Joachims-MacBook-Pro.local>
2017-06-15 14:51:24 +0200

    Filling it in a (in my opinion) very complicated manner with the help of necessary init-arguments and arrays of CellContentData

---
+++ myTableViewTutorial_1_170614/myCellView.swift
@@ -0,0 +1,19 @@
+//
+//  myCellView.swift
+//  myTableViewTutorial_1_170614
+//
+//  Created by Joachim Vetter on 15.06.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class myCellView: UITableViewCell {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ myTableViewTutorial_1_170614/myData.swift
@@ -0,0 +1,18 @@
+//
+//  myData.swift
+//  myTableViewTutorial_1_170614
+//
+//  Created by Joachim Vetter on 15.06.17.
+//  Copyright © 2017 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class myData {
+    var puzzles = String()
+    var checkmarks = Bool()
+    init(puzzles: String, checkmarks: Bool) {
+        self.puzzles = puzzles
+        self.checkmarks = checkmarks
+    }
+}
